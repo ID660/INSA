7db43354b58030dcb05dbeb12df49593715e1cc0
DRAMÉ INSA <insdrame236@gmail.com>
2021-09-27 16:20:35 +0200

    Add files via upload

---
+++ virtualtalking/main.py
@@ -0,0 +1,10 @@
+import pyttsx3
+friend = pyttsx3.init()
+friend.say('im beautifull an intelligent and very rich im billionar')
+friend.runAndWait()
+
+import pyttsx3
+friend = pyttsx3.init()
+speech = input("say something")
+friend.say(speech)
+friend.runAndWait()
